e83d7b676ef38185df29f3cc0db6b66ce99c2a21
ulomae <ulomaemeh@gmail.com>
2023-10-08 19:19:23 -0700

    added sql database to store scraped data

--- main.py
+++ main.py
@@ -8,67 +8,78 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import sqlite3
+
+conn = sqlite3.connect('Intern search.db')
+c = conn.cursor()
+
+# c.execute('''CREATE TABLE internships(Job postings TEXT)''')
 
 options = Options()
-options.add_experimental_option("detach", True)
+options.add_experimental_option('detach', True)
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 
-driver.get("https://www.indeed.com/")
+driver.get('https://www.indeed.com/')
 driver.maximize_window()
 time.sleep(3)
 
 
-search_job = driver.find_element(By.ID, "text-input-what")
-search_place = driver.find_element(By.ID, "text-input-where")
+search_job = driver.find_element(By.ID, 'text-input-what')
+search_place = driver.find_element(By.ID, 'text-input-where')
 
-search_job.send_keys(Keys.CONTROL + "a")
+search_job.send_keys(Keys.CONTROL + 'a')
 search_job.send_keys(Keys.DELETE)
 
-search_place.send_keys(Keys.CONTROL + "a")
+search_place.send_keys(Keys.CONTROL + 'a')
 search_place.send_keys(Keys.DELETE)
 
-search_job.send_keys("Software Intern")
-search_place.send_keys("Irvine, CA")
+search_job.send_keys('Software Intern')
+search_place.send_keys('Irvine, CA')
 search_place.send_keys(Keys.RETURN)
 time.sleep(2)
 
 try:
     date_button = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.ID, "filter-dateposted"))
+        EC.presence_of_element_located((By.ID, 'filter-dateposted'))
     )
     date_button.click()
 except:
     driver.quit()
 
-date_filter = driver.find_element(By.LINK_TEXT, "Last 24 hours")
+date_filter = driver.find_element(By.LINK_TEXT, 'Last 24 hours')
 date_filter.click()
 '''
 try:
     miles_button = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.ID, "filter-radius"))
+        EC.presence_of_element_located((By.ID, 'filter-radius'))
     )
     miles_button.click()
 except:
     driver.quit()
 
-miles_filter = driver.find_element(By.LINK_TEXT, "Within 25 miles")
+miles_filter = driver.find_element(By.LINK_TEXT, 'Within 25 miles')
 miles_filter.click()
 '''
 
 try:
     job_page = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.ID, "mosaic-jobResults"))
+        EC.presence_of_element_located((By.ID, 'mosaic-jobResults'))
     )
-    newJobs = job_page.find_elements(By.CLASS_NAME, "cardOutline")
+    newJobs = job_page.find_elements(By.CLASS_NAME, 'cardOutline')
     for newJob in newJobs:
-        job_info = newJob.find_element(By.CLASS_NAME, "jobCard_mainContent")
-        print(job_info.text)
+        job_info = newJob.find_element(By.CLASS_NAME, 'jobCard_mainContent')
+        job_info = job_info.text
+        c.execute('''INSERT INTO internships VALUES(?)''',(job_info))
 
+        # print(job_info)
 finally:
     driver.quit()
 
-
-
-
+conn.commit()
+print('complete')
+#
+#
+#
+#
