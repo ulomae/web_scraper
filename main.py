3006921049bb5c6bd3659e11f6db463b9ab87502
ulomae <ulomaemeh@gmail.com>
2023-10-08 17:43:35 -0700

    printed out search results

--- main.py
+++ main.py
@@ -34,19 +34,40 @@ search_place.send_keys("Irvine, CA")
 search_place.send_keys(Keys.RETURN)
 time.sleep(2)
 
-date_button = driver.find_element(By.ID, "filter-dateposted")
-date_button.click()
+try:
+    date_button = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, "filter-dateposted"))
+    )
+    date_button.click()
+except:
+    driver.quit()
 
 date_filter = driver.find_element(By.LINK_TEXT, "Last 24 hours")
 date_filter.click()
-time.sleep(2)
-miles_button = driver.find_element(By.ID, "filter-radius")
-miles_button.click()
+'''
+try:
+    miles_button = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, "filter-radius"))
+    )
+    miles_button.click()
+except:
+    driver.quit()
 
 miles_filter = driver.find_element(By.LINK_TEXT, "Within 25 miles")
 miles_filter.click()
+'''
 
+try:
+    job_page = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, "mosaic-jobResults"))
+    )
+    newJobs = job_page.find_elements(By.CLASS_NAME, "cardOutline")
+    for newJob in newJobs:
+        job_info = newJob.find_element(By.CLASS_NAME, "jobCard_mainContent")
+        print(job_info.text)
 
+finally:
+    driver.quit()
 
 
 
